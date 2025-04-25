23328e023f2dcdba9cb34a809a72a2428ad99de9
unknown <hmstech11@gmail.com>
2025-04-25 16:05:44 +0500

    helo wolrd:

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable
     }
     public function sendParcel()
     {
-        return $this->hasMany(SendParcel::class, 'user_id');
+        return $this->hasMany(SendParcel::class);
     }
 
     public function sentMessages()
