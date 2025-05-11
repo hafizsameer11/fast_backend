fa208ad002b0d83fa07fb367c23f393633c51798
unknown <hmstech11@gmail.com>
2025-05-11 19:11:22 +0500

    hello wolrd

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,4 +77,7 @@ class User extends Authenticatable
     {
         return $this->hasOne(Wallet::class);
     }
+    public function riderVerification(){
+        return $this->hasOne(RiderVerification::class);
+    }
 }
