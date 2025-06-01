584802a4154de0deb33371dcfbb6aa6e60632168
HamzaBilal2003 <hamza@hmstech.org>
2025-06-01 18:27:13 +0500

    done

---
+++ database/migrations/2025_06_01_132625_update_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // $table->enum('role', ['admin', 'user', 'rider', 'agent'])->default('user'); change type enum to string
+            $table->string('role')->default('user')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
